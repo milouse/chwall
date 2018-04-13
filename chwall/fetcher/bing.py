cd7e23ba7c4beec5a91ff153aa52bcd6e9b36f4e
Étienne Deparis <etienne@depar.is>
2018-04-13 18:49:13 +0200

    Add a default behavior for bing fetcher

--- chwall/fetcher/bing.py
+++ chwall/fetcher/bing.py
@@ -5,13 +5,15 @@ import requests
 
 
 def fetch_pictures(config):
-    if "bing" not in config:
-        return {}
     collecs = {}
+    already_done = []
     url = "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0" \
           "&n=10&mkt={}"
-    already_done = []
-    for l in config["bing"]:
+    if "bing" in config:
+        i18n_src = config["bing"]
+    else:
+        i18n_src = ['en-US', 'fr-FR']
+    for l in i18n_src:
         lu = "{}[0-9]{{10}}".format(l.upper())
         data = requests.get(url.format(l)).json()
         for p in data["images"]:
