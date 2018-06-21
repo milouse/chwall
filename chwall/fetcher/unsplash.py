7553f8e96d19155a67d9b3070e1830c05340ddc4
Étienne Deparis <etienne@depar.is>
2018-06-21 12:47:58 +0200

    feat(unsplash): Support Unsplash API
    This commit allow one to use pictures from any public collections or to
    query Unsplash API, instead of just relying of the obscure /rss
    endpoint.

--- chwall/fetcher/unsplash.py
+++ chwall/fetcher/unsplash.py
@@ -6,12 +6,8 @@ from xml.etree import ElementTree
 from urllib.parse import urlsplit
 
 
-def fetch_pictures(config):
+def fetch_from_rss(config, width):
     collecs = {}
-    width = 1600
-    if "unsplash" in config and "width" in config["unsplash"]:
-        width = config["unsplash"]["width"]
-
     data = ElementTree.fromstring(
         requests.get("https://unsplash.com/rss").text)
     for item in data[0].findall("item"):
@@ -33,7 +29,49 @@ def fetch_pictures(config):
                       data, re.MULTILINE)
         if m is None:
             continue
-        tp["copyright"] = "Picture by {}".format(m[3])
+        tp["copyright"] = "Picture by {} (on Unsplash)".format(m[3])
         tp["url"] = m[1]
         collecs[tp["image"]] = tp
+    return collecs
+
+
+def fetch_pictures(config):
+    width = 1600
+    params = ["w=1600"]
+    client_id = None
+    if "unsplash" in config:
+        if "width" in config["unsplash"]:
+            width = config["unsplash"]["width"]
+            params[0] = "w=%d" % width
+        if "access_key" in config["unsplash"]:
+            client_id = config["unsplash"]["access_key"]
+        if "count" in config["unsplash"]:
+            params.append("count=%d" % config["unsplash"]["count"])
+        if "query" in config["unsplash"]:
+            params.append("query=" + config["unsplash"]["query"])
+        if "collections" in config["unsplash"]:
+            params.append(
+                "collections=" + ",".join(config["unsplash"]["collections"]))
+    if client_id is None:
+        return fetch_from_rss(config, width)
+    url = "https://api.unsplash.com/photos/random"
+    params.append("client_id=" + client_id)
+    collecs = {}
+    data = requests.get("{}?{}".format(url, "&".join(params))).json()
+    for p in data:
+        px = p["urls"]["custom"]
+        if p["description"] is None:
+            label = "Picture"
+        else:
+            label = p["description"]
+        label = "{} by {} (on Unsplash)".format(label, p["user"]["name"])
+        if "location" in p and p["location"]["title"] is not None:
+            label = "{}, taken in {}".format(label, p["location"]["title"])
+        collecs[px] = {
+            "image": px,
+            "copyright": label,
+            "url": p["links"]["html"],
+            "type": "unsplash",
+            "local": False
+        }
     return collecs
